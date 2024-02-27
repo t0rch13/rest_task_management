3abd408bdd8267c1130def57585ba01c26162555
t0rch13 <batyrhan22.11@gmail.com>
2024-02-27 23:39:36 +0600

    fix: adminRoutes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,9 +7,11 @@ const router = express.Router();
 router.get('/admin', authenticate, controller.getAdmin);
 router.get('/admin/users', authenticate, controller.getUsers);
 router.get('/admin/users/:id', authenticate, controller.getUser);
-router.get('/admin/users/:id/delete', authenticate, controller.deleteUser);
+router.delete('/admin/users/:id/delete', authenticate, controller.deleteUser);
 router.get('/admin/users/:id/edit', authenticate, controller.getEditUser);
 router.post('/admin/users/:id/edit', authenticate, controller.postEditUser);
+
+module.exports = router;
 
 
 
